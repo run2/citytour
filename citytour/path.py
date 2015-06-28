de2682bdefa2b3cd26032dafe1c61f7bf35f1f85
run2 <b.debanjan@gmail.com>
2015-06-28 10:58:52 +0530

    adding logging

---
+++ citytour/path.py
@@ -0,0 +1,14 @@
+def set_city_map():
+    return(u'City map set')
+
+def set_constraints():
+    logging.debug('Setting constraints')
+    return(u'Constraints set')
+
+def joke():
+    logging.info('Here comes the shortest joke')
+    return (u'2 women are sitting quite')
+
+def fastest_between_nodes():
+    logging.warning('There is no intelligence there yet')
+    return (u'default')

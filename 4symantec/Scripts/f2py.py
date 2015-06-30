1e4b7d85f597b9677b6e1f3cad8cf2037eeb1ad3
run2 <b.debanjan@gmail.com>
2015-06-30 16:35:03 +0530

    adding numpy to virtualenv

---
+++ 4symantec/Scripts/f2py.py
@@ -0,0 +1,4 @@
+#!C:\citytour\4symantec\Scripts\python.exe
+# EASY-INSTALL-SCRIPT: 'numpy==1.9.2','f2py.py'
+__requires__ = 'numpy==1.9.2'
+__import__('pkg_resources').run_script('numpy==1.9.2', 'f2py.py')
